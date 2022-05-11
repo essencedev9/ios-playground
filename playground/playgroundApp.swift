f6a97622977c0ca11a3f99ffce6237c3025a536b
essencedev9 <essencedev9@nntuple.com>
2022-05-11 10:43:51 +0900

    Initial Commit

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by jeong on 2022/05/11.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
